10728159bf89d75cb7936f11cc093320aaf500b8
Mahmoud Galal <mahmoudgalal69@gmail.com>
2022-12-12 02:03:35 +0200

    4v

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors")
 const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
 
@@ -7,6 +8,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.get("/", (req, res) => {
